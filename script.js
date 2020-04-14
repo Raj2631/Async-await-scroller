58f42ecb5d0682186fff34aa8b99cafd4b9550ef
Rakshit Pathak <rajatpathak435@gmail.com>
2020-04-14 08:31:08 +0530

    added infinite scrolling

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 // Fetch posts from API.
@@ -32,4 +32,24 @@ async function showPosts() {
   });
 }
 
+// Show loader and fetch more posts.
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+    setTimeout(() => {
+      page++;
+      showPosts();
+    });
+  }, 600);
+}
+
 showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
